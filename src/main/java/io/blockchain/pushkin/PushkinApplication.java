1492ae4266bd808353b3d2bb6b35e00688ab7e2b
7dev7 <alexandrovich.anton7@gmail.com>
2018-03-23 20:40:21 +0300

    added root bot

--- src/main/java/io/blockchain/pushkin/PushkinApplication.java
+++ src/main/java/io/blockchain/pushkin/PushkinApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PushkinApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PushkinApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PushkinApplication.class, args);
+    }
 }
