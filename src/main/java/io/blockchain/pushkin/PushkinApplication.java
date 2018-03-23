7451df278a1f969314d6dedd12cf2978f45692c4
Dmitriy Baboshkin <Daemon712@yandex.ru>
2018-03-23 20:19:17 +0400

    Initial commit

---
+++ src/main/java/io/blockchain/pushkin/PushkinApplication.java
@@ -0,0 +1,12 @@
+package io.blockchain.pushkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushkinApplication.class, args);
+	}
+}
