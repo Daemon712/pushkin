1a3701430360e1fca32e21b7f11a55588b79d010
DenisZaichenko <svetlograd9@gmail.com>
2018-03-25 13:01:48 +0400

    Загрузка с хакатоновского чатика

--- src/main/java/io/blockchain/pushkin/ChatLoader.java
+++ src/main/java/io/blockchain/pushkin/ChatLoader.java
@@ -45,8 +45,9 @@ public class ChatLoader {
         this.wordUsageRepository = wordUsageRepository;
     }
 
-    //@PostConstruct
+    // @PostConstruct
     public void loadMessages() {
+        loadHMessages();
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         final Pattern pattern = Pattern.compile("(-\\d*),(\\d*),(\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d).216,\"?([^\\>]*)\"?,(\\d*)");
         try (BufferedReader br = new BufferedReader(new FileReader(new File(Paths.get("src", "main", "resources", "messages").toString())))) {
@@ -58,7 +59,6 @@ public class ChatLoader {
                     try {
                         if (matcher.group(4).trim().equals(""))
                             continue;
-                        System.out.println(matcher.group(4));
                         int userId = Integer.parseInt(matcher.group(5));
                         int messageId = Integer.parseInt(matcher.group(2));
                         MessageEntity messageEntity = new MessageEntity();
@@ -69,6 +69,56 @@ public class ChatLoader {
                         messageEntity.setUser(new TgUser(userId, ""));
                         messageEntity.setText(matcher.group(4));
                         messageEntity.setDate(formatter.parse(matcher.group(3)));
+
+                        List<Word> lemmas = lemmaService.getLemmas(matcher.group(4));
+
+                        // Spell checking
+                        Double literacy = spellCheckerService.checkMessage(messageEntity.getText(), lemmas.size());
+                        if (literacy != null) {
+                            messageEntity.setLiteracy(literacy);
+                        }
+
+                        messageEntityRepository.save(messageEntity);
+
+                        List<WordUsage> wordUsageList = IntStream.range(0, lemmas.size())
+                                .mapToObj(i -> new WordUsage(new WordUsagePK(messagePK, i), lemmas.get(i)))
+                                .peek(wordUsage -> wordUsage.setMessage(messageEntity))
+                                .collect(Collectors.toList());
+
+                        wordUsageRepository.saveAll(wordUsageList);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadHMessages() {
+        int messId = 1;
+        int CHAT_ID = 123123;//TODO Заменить на ID реального чата
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(Paths.get("src", "main", "resources", "messages.txt").toString())))) {
+            String line;
+            SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+            Pattern pattern = Pattern.compile("([0-9][0-9].[0-9][0-9].[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]), ([\\w ]*)[\\d\\w _]*\\[[\\d\\w _]*\\|(\\w*)\\]: ([^\\>]*)");
+            while ((line = br.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+
+                if (matcher.find()) {
+                    try {
+                        int userId = Integer.parseInt(matcher.group(3));
+                        MessageEntity messageEntity = new MessageEntity();
+                        MessagePK messagePK = new MessagePK(CHAT_ID, messId++);
+                        messageEntity.setMessagePK(messagePK);
+
+
+                        messageEntity.setUser(new TgUser(userId, matcher.group(2)));
+                        messageEntity.setText(matcher.group(4));
+                        messageEntity.setDate(formatter.parse(matcher.group(1)));
 
                         List<Word> lemmas = lemmaService.getLemmas(matcher.group(4));
 
