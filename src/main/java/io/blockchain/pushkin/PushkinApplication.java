5bfe5f8ef073c39489ca01d7e11edee43563dd9b
Dmitriy Baboshkin <Daemon712@yandex.ru>
2018-03-31 16:55:49 +0400

    make JLanguageTool thread local

--- src/main/java/io/blockchain/pushkin/PushkinApplication.java
+++ src/main/java/io/blockchain/pushkin/PushkinApplication.java
@@ -4,10 +4,13 @@ import okhttp3.OkHttpClient;
 import org.languagetool.JLanguageTool;
 import org.languagetool.language.Russian;
 import org.languagetool.rules.CategoryId;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.SimpleThreadScope;
 
 @Configuration
 @SpringBootApplication
@@ -22,9 +25,20 @@ public class PushkinApplication {
     }
 
     @Bean
+    public static BeanFactoryPostProcessor beanFactoryPostProcessor() {
+        return beanFactory -> beanFactory.registerScope("thread", new SimpleThreadScope());
+    }
+
+    @Bean
+    @Scope("thread")
     public JLanguageTool jLanguageTool() {
-        JLanguageTool jLanguageTool = new JLanguageTool(new Russian());
+        JLanguageTool jLanguageTool = new JLanguageTool(language());
         jLanguageTool.disableCategory(new CategoryId("CASING"));
         return jLanguageTool;
+    }
+
+    @Bean
+    public Russian language() {
+        return new Russian();
     }
 }
